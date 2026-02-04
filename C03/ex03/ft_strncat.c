0b276cba01dc6015d7e7d467fc287a70b8b45b4f
Felipi Forbeci Freo <felforbe@c3r12p5.42sp.org.br>
2026-02-04 20:10:00 -0300

    C05 Finalizada

---
+++ C03/ex03/ft_strncat.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: felforbe <felforbe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/01 17:25:29 by felforbe          #+#    #+#             */
+/*   Updated: 2026/02/01 17:55:55 by felforbe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i_dest;
+	unsigned int	i_src;
+
+	i_dest = 0;
+	i_src = 0;
+	while (dest[i_dest])
+	{
+		i_dest++;
+	}
+	while (src[i_src] && i_src < nb)
+	{
+		dest[i_dest] = src[i_src];
+		i_dest++;
+		i_src++;
+	}
+	dest[i_dest] = '\0';
+	return (dest);
+}
