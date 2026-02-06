9667a15c70c82b867e22e6de0263a0838497d44f
Felipi Forbeci Freo <felforbe@c1r8p4.42sp.org.br>
2026-02-06 02:05:30 -0300

    C05 - Finalizada

---
+++ C05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: felforbe <felforbe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/04 20:33:24 by felforbe          #+#    #+#             */
+/*   Updated: 2026/02/04 20:46:40 by felforbe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_factorial(int nb)
+{
+	int	result;
+	int	i;
+
+	if (nb < 0)
+		return (0);
+	result = 1;
+	i = 1;
+	while (i <= nb)
+	{
+		result *= i;
+		i++;
+	}
+	return (result);
+}
---
+++ C05/ex02/ft_iterative_power.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: felforbe <felforbe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/04 20:51:16 by felforbe          #+#    #+#             */
+/*   Updated: 2026/02/04 20:55:36 by felforbe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_power(int nb, int power)
+{
+	int	result;
+
+	result = 1;
+	if (power < 0)
+		return (0);
+	if (nb == 0 && power == 0)
+		return (1);
+	while (power > 0)
+	{
+		result *= nb;
+		power--;
+	}
+	return (result);
+}
