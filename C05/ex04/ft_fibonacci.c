d73b17bd7e34855887586b8cc70b7c6fd9d940cf
Felipi Forbeci Freo <felforbe@c1r8p4.42sp.org.br>
2026-02-06 02:07:37 -0300

    C05 - Finalizada

---
+++ C05/ex04/ft_fibonacci.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: felforbe <felforbe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/04 21:01:18 by felforbe          #+#    #+#             */
+/*   Updated: 2026/02/04 21:13:22 by felforbe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index == 0)
+		return (0);
+	if (index == 1)
+		return (1);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
